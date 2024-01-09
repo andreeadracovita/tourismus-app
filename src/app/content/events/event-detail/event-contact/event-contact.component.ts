5d3ccde192bea4152780eba64496e8ad84a28923
Andreea Racovita <andreearacovitagit@gmail.com>
2024-01-09 13:55:45 +0200

    Add event detail content

--- src/app/content/events/event-detail/event-contact/event-contact.component.ts
+++ src/app/content/events/event-detail/event-contact/event-contact.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-event-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './event-contact.component.html',
   styleUrl: './event-contact.component.css'
 })
 export class EventContactComponent {
-
+  today = new Date();
 }
