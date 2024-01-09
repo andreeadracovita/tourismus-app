82892cf633c024c46f3037fd98f00fac4d211a06
Andreea Racovita <andreearacovitagit@gmail.com>
2024-01-09 08:51:51 +0200

    Draft search component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    {provide: MAT_DATE_LOCALE, useValue: 'de-CH'}
+  ]
 };
--- src/app/content/search/search.component.ts
+++ src/app/content/search/search.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  searchValue: string = '';
+  selectedDate: Date | null = new Date();
+  selectedCategories: string[] = ['All'];
+  
+  categories: string[] = ['All', 'Exhibitions', 'Sports & Fitness', 'Classical Music', 'Party', 'Fair & Market'];
 
+  onCheckboxChange(category: string, event: any) {
+    let isChecked = false;
+    if (event.target.checked) {
+      isChecked = true;
+    }
+    if(isChecked === true) {
+      this.selectedCategories.push(category);
+    } else {
+      const index = this.selectedCategories.indexOf(category, 0);
+      if (index > -1) {
+        this.selectedCategories.splice(index, 1);
+      }
+    }
+    console.log(this.selectedCategories);
+  }
 }
